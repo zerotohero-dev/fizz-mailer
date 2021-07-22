d3a51fe20d761787b2115ed174d73e28d66eceec
Volkan Ozcelik <me@volkan.io>
2021-07-22 04:26:07 -0700

    getting path prefix from env

--- internal/api/api.go
+++ internal/api/api.go
@@ -22,10 +22,10 @@ import (
 	"github.com/zerotohero-dev/fizz-mailer/internal/transport"
 )
 
-const prefix = "/mailer"
-
 func InitializeEndpoints(e env.FizzEnv, router *mux.Router) {
 	svc := service.New(e, context.Background())
+
+	prefix := e.Mailer.PathPrefix
 
 	// Sends email verification email.
 	app.RoutePrefixedPath(
