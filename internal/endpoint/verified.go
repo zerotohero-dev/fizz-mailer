3c8728e4f46cac097705612e2fe6a4aa831c4e13
Volkan Ozcelik <me@volkan.io>
2021-08-15 08:51:56 -0700

    recent changes

--- internal/endpoint/verified.go
+++ internal/endpoint/verified.go
@@ -14,62 +14,64 @@ package endpoint
 import (
 	"context"
 	"github.com/go-kit/kit/endpoint"
-	"github.com/zerotohero-dev/fizz-entity/pkg/reqres"
 	"github.com/zerotohero-dev/fizz-mailer/internal/service"
-	"github.com/zerotohero-dev/fizz-validation/pkg/sanitization"
 )
 
 func MakeRelayEmailVerifiedMessageEndpoint(svc service.Service) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
-		if gr, hasProblem := request.(reqres.ContentTypeProblemRequest); hasProblem {
-			return reqres.RelayEmailVerifiedMessageResponse{
-				Success: false,
-				Err:     gr.Err,
-			}, nil
-		}
+		// TODO: delete me; if it is not being used anywhere.
 
-		req := request.(reqres.RelayEmailVerifiedMessageRequest)
-		if req.Err != "" {
-			return reqres.RelayEmailVerifiedMessageResponse{
-				Success: false,
-				Err:     req.Err,
-			}, nil
-		}
+		panic("I will likely delete this endpoint.")
 
-		email := sanitization.SanitizeEmail(req.Email)
-		if email == "" {
-			return reqres.RelayEmailVerifiedMessageResponse{
-				Success: false,
-				Err:     "Error: missing email",
-			}, nil
-		}
-
-		name := sanitization.SanitizeName(req.Name)
-		if name == "" {
-			return reqres.RelayEmailVerifiedMessageResponse{
-				Success: false,
-				Err:     "Error: missing full name",
-			}, nil
-		}
-
-		token := sanitization.SanitizeToken(req.Token)
-		if token == "" {
-			return reqres.RelayEmailVerifiedMessageResponse{
-				Success: false,
-				Err:     "Error: missing token",
-			}, nil
-		}
-
-		err := svc.RelayEmailVerifiedMessage(email, token, name)
-		if err != nil {
-			return reqres.RelayEmailVerifiedMessageResponse{
-				Success: false,
-				Err:     "Error sending user verification email",
-			}, nil
-		}
-
-		return reqres.RelayEmailVerifiedMessageResponse{
-			Success: true,
-		}, nil
+		//if gr, hasProblem := request.(reqres.ContentTypeProblemRequest); hasProblem {
+		//	return reqres.RelayEmailVerifiedMessageResponse{
+		//		Success: false,
+		//		Err:     gr.Err,
+		//	}, nil
+		//}
+		//
+		//req := request.(reqres.RelayEmailVerifiedMessageRequest)
+		//if req.Err != "" {
+		//	return reqres.RelayEmailVerifiedMessageResponse{
+		//		Success: false,
+		//		Err:     req.Err,
+		//	}, nil
+		//}
+		//
+		//email := sanitization.SanitizeEmail(req.Email)
+		//if email == "" {
+		//	return reqres.RelayEmailVerifiedMessageResponse{
+		//		Success: false,
+		//		Err:     "Error: missing email",
+		//	}, nil
+		//}
+		//
+		//name := sanitization.SanitizeName(req.Name)
+		//if name == "" {
+		//	return reqres.RelayEmailVerifiedMessageResponse{
+		//		Success: false,
+		//		Err:     "Error: missing full name",
+		//	}, nil
+		//}
+		//
+		//token := sanitization.SanitizeToken(req.Token)
+		//if token == "" {
+		//	return reqres.RelayEmailVerifiedMessageResponse{
+		//		Success: false,
+		//		Err:     "Error: missing token",
+		//	}, nil
+		//}
+		//
+		//err := svc.RelayEmailVerifiedMessage(email, token, name)
+		//if err != nil {
+		//	return reqres.RelayEmailVerifiedMessageResponse{
+		//		Success: false,
+		//		Err:     "Error sending user verification email",
+		//	}, nil
+		//}
+		//
+		//return reqres.RelayEmailVerifiedMessageResponse{
+		//	Success: true,
+		//}, nil
 	}
 }
