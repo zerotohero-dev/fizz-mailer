58aa50da2f9618ea87c4596847de799aca872293
Volkan Ozcelik <me@volkan.io>
2021-08-28 13:54:46 -0700

    recent

---
+++ hack/build-dev.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+#  \
+#  \\,
+#   \\\,^,.,,.                     Zero to Hero
+#   ,;7~((\))`;;,,               <zerotohero.dev>
+#   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+#    )  . ),((  ))\;,
+#   /;`,,/7),)) )) )\,,
+#  (& )`   (,((,((;( ))\,
+
+go build cmd/main.go
